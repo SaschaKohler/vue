aeb993d59693190f98902b67a66d33034437a2eb
SaschaKohler <sascha.kohler@sent.at>
2021-10-21 14:38:45 +0200

    starting with vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="text-center">
+    <div v-for="airline in airlines" v-bind:key="airline.id">
+      <h2>{{ airline.name }}</h2>
+      <img class="w-100 rounded-full" v-bind:src="airline.logo" />
+    </div>
+    <h1>fun</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      airlines: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        const response = await this.$http.get(
+            "http://localhost/api/airlines"
+        );
+        this.airlines = response.data
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+  },
+
+  created() {
+    this.getData();
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
